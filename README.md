7e9a725f0edfe57b540cf74347a82184df05056c
francocarballar <francocarballar@gmail.com>
2022-05-05 23:07:12 -0300

    Primer commit del proyecto Google Drive

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# clon-de-google-drive
+# Clon de Google Drive
 Google Drive es un servicio de alojamiento de archivos que fue introducido por la empresa estadounidense Google el 24 de abril de 2012. Antes llamado Google Docs que ha cambiado su dirección URL, entre otras cosas. Es uno de los sitios de alojamiento más conocidos en el mundo.
+
+Clon de Google Drive, es una proyecto creado para prácticar los conceptos del desarrollo web. Su única intención es imitar la apariencia y el funcionamiento de www.drive.google.com, no tiene como objetivo reemplazar o sustituir la página oficial de Google Drive.
+## Autores 
+
+Franco Carballar [@francocarballar](https://github.com/francocarballar)
+Juan Espínola [@juanespinola05](https://github.com/juanespinola05)
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugins');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        })
+    ]
+}
