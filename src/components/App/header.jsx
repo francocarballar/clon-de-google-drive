dbb9cb684d07a7f393bb7d84dc2cdc1cb9ece244
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-05-11 09:54:28 -0300

    Style: Formatted files

--- src/components/App/header.jsx
+++ src/components/App/header.jsx
@@ -1,43 +1,43 @@
-import React, { useState } from 'react';
-import './header.css';
-import menuIcon from '../../assets/icons/menu_icon.svg';
-import searchIcon from '../../assets/icons/search_icon.svg';
-import viewIcon from '../../assets/icons/view_icon.svg';
-import threePointsIcon from '../../assets/icons/three-points_icon.svg';
+import React, { useState } from 'react'
+import './header.css'
+import menuIcon from '../../assets/icons/menu_icon.svg'
+import searchIcon from '../../assets/icons/search_icon.svg'
+import viewIcon from '../../assets/icons/view_icon.svg'
+import threePointsIcon from '../../assets/icons/three-points_icon.svg'
 
-function Header ({state, setState}) {
-    return (
-        <React.Fragment>
-            <header className="header">
-                <nav className="header__nav">
-                    <ul className="header__ul">
-                        <div className="header__container--menu">
-                            <li>
-                                <img 
-                                    src={menuIcon} 
-                                    alt='icon menu' 
-                                    id='header__menu--icon' 
-                                    onClick={(e) => {setState(!state)}}
-                                />
-                            </li>
-                            <li>Mi unidad</li>
-                        </div>
-                        <div>
-                            <li>
-                                <img src={searchIcon} alt='' />
-                            </li>
-                            <li>
-                                <img src={viewIcon} alt='' />
-                            </li>
-                            <li>
-                                <img src={threePointsIcon} alt='' />
-                            </li>
-                        </div>
-                    </ul>
-                </nav>
-            </header>
-        </React.Fragment>
-    );
+function Header ({ state, setState }) {
+  return (
+    <>
+      <header className='header'>
+        <nav className='header__nav'>
+          <ul className='header__ul'>
+            <div className='header__container--menu'>
+              <li>
+                <img
+                  src={menuIcon}
+                  alt='icon menu'
+                  id='header__menu--icon'
+                  onClick={(e) => { setState(!state) }}
+                />
+              </li>
+              <li>Mi unidad</li>
+            </div>
+            <div>
+              <li>
+                <img src={searchIcon} alt='' />
+              </li>
+              <li>
+                <img src={viewIcon} alt='' />
+              </li>
+              <li>
+                <img src={threePointsIcon} alt='' />
+              </li>
+            </div>
+          </ul>
+        </nav>
+      </header>
+    </>
+  )
 }
 
-export {Header};
+export { Header }
--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,23 +1,23 @@
-import React, {useState} from 'react';
-import './App.css';
-import {Header} from './header';
-import {Sidebar} from './sidebar';
+import React, { useState } from 'react'
+import './App.css'
+import { Header } from './header'
+import { Sidebar } from './sidebar'
 
 function App () {
-  const [stateSidebar, setStateSidebar] = useState(false);
+  const [stateSidebar, setStateSidebar] = useState(false)
   return (
-    <React.Fragment>
+    <>
       <Header
         state={stateSidebar}
         setState={setStateSidebar}
       />
-      <Sidebar 
+      <Sidebar
         state={stateSidebar}
         setState={setStateSidebar}
       />
       <h1>Hello Google Drive Clone</h1>
-    </React.Fragment>
-  );
+    </>
+  )
 }
 
 export { App }
